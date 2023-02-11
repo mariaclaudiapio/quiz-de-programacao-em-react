56b2ed5e4d0fabfdbdf08a90a79deed6105ef5f5
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-11 18:17:07 -0300

    Desenvolvimento da lógica de perguntas:
    apresentando a pergunta na tela;
    embaralhando as perguntas antes da exibição

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -1,7 +1,25 @@
+import { useContext } from "react";
+
 import "./Question.css";
+import { QuizContext } from "../../context/quiz";
 
 const Question = () => {
-  return <div>Question</div>;
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  const currentQuestion = quizState.questions[quizState.currentQuestion];
+
+  return (
+    <div id="question">
+      <p>
+        Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}
+      </p>
+      <h2>{currentQuestion.question}</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
+    </div>
+  );
 };
 
 export default Question;
